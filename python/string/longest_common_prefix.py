0d9a92a74b804c3263b0bbad6f1d667db77a2516
toannt52 <toannt52@fpt.com>
2025-08-26 14:54:11 +0700

    Longest Common Prefix solution 2

--- python/string/longest_common_prefix.py
+++ python/string/longest_common_prefix.py
@@ -4,21 +4,19 @@ from typing import List
 Link problem: https://leetcode.com/problems/longest-common-prefix/
 """
 class Solution:
+    # ["flop", "fly", "flower"]
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if len(strs) == 0:
             return ""
         
-        common_word = min(strs, key=len)
-        result = ""
+        common_word = min(strs, key=len) #"fly"
+        # compare "fly" forward
         for i in range(len(common_word)):
-            c = common_word[i]
-            for index, word in enumerate(strs):
-                if word[i] == c:
-                  if index == len(strs) - 1:
-                      result += word[i]
-                else:
-                    return result
-        return result
+            for word in strs:
+                if(common_word[i] != word[i]):
+                    return common_word[:i]
+        return common_word
+            
             
 s = Solution()
 print(s.longestCommonPrefix(["flower","flow","flight"]))  # Output: "fl"
